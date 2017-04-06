f197459c5b1908cd01df5cead97248505bd41709
Daniel King <daniel.oliver.king@gmail.com>
2017-04-05 19:56:57 -0700

    Add Mock API to simulate data

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.get('/roles', (req, res) => {
   res.json({
-    roles: [
-      { name: 'hello' }
-    ]
+    roles: [{ name: 'hello' }]
   });
 });
 
